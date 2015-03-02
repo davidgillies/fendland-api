d65f7bc3ee55104188bd643bf9cd1d6703511d2c
David Gillies <david.gillies.rhu@gmail.com>
2015-03-02 15:42:50 +0000

    added initial view classes and application model

---
+++ fenland_api/api_renderer/cam_apps.py
@@ -0,0 +1,48 @@
+from lxml import objectify
+import sqlsoup
+import simplejson
+
+
+class Application(object):
+    def __inti__(self, name, xml):
+        self.name = name
+        self.xml = xml
+        self.xml_object = objectify.from_string(self.xml)
+        self.db = sqlsoup.SQLSoup('mysql+pymysql://david:david@localhost:3306/sm_db')
+
+    def get_data(self, section_number, id_variable, id_variable_value):
+        self.db.table = self.db.entity(self.get_table_name(section_number))
+        data = self.db.table.get(int(id_variable_value)).__dict__
+        data.pop('_sa_instance_state')
+        return data
+
+    def insert_data(self, section_number, id_variable, id_variable_value, body):
+        self.db.table = self.db.entity(self.get_table_name(section_number))
+        json_dict = simplejson.JSONDecoder().decode(body)
+        data = self.db.table.insert(**json_dict).__dict__
+        data.pop('_sa_instance_state')
+        self.db.commit()
+        return data
+
+    def update_data(self, section_number, id_variable, id_variable_value, body):
+        self.db.table = self.db.entity(self.get_table_name(section_number))
+        json_dict = simplejson.JSONDecoder().decode(body)
+        # problem: can't put a variable into this filter_by must be a
+        # database column name
+        data = self.db.table.filter_by(id=int(id_variable_value)).update(json_dict)
+        data = json_dict
+        self.db.commit()
+        return data
+
+    def delete_data(self, section_number, id_variable, id_variable_value):
+        self.db.table = self.db.entity(self.get_table_name(section_number))
+        instance = self.db.table.get(int(id_variable_value))
+        self.db.delete(instance)
+        self.db.commit()
+        return
+
+    def get_table_name(self, section_number):
+        return 'volunteers'
+
+    def get_section(self, section_number):
+        return self.xml_object.section[int(section_number)]
--- fenland_api/html_renderer/views.py
+++ fenland_api/html_renderer/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import View
+from cam_apps import Application
 
-# Create your views here.
+xml_string = open('U:/Data/forms_api/forms_api/xmlfiles/Fenland.xml', 'r').read()
+fenland_app = Application('fenland', xml_string)
+
+class HTMLView(View):
+    pass
