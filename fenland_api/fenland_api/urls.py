ac1bdec025544199c6533708cb097d17c7843642
David Gillies <david.gillies.rhu@gmail.com>
2015-03-02 16:28:39 +0000

    added generic url set up to urls

--- fenland_api/fenland_api/urls.py
+++ fenland_api/fenland_api/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from html_renderer import views as html_renderer_views
+from api_renderer import views as api_renderer_views
 from django.views.decorators.csrf import csrf_exempt
 
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-#    url(r'^a/', csrf_exempt(html_renderer_views.HTMLView.as_view()) ),
-#   url(r'^b/', 'api_renderer.views.tester'),
+    url(r'^html/(\w+)/(\w+)/(\w+)', csrf_exempt(html_renderer_views.HTMLView.as_view())),
+    url(r'^html/(\w+)/(\w+)', csrf_exempt(html_renderer_views.HTMLView.as_view())),
+    url(r'^html/(\w+)', csrf_exempt(html_renderer_views.HTMLView.as_view())),
+    urlr'^html/', csrf_exempt(html_renderer_views.HTMLView.as_view())),
+    url(r'^api/(\w+)/(\w+)/(\w+)', csrf_exempt(api_renderer_views.APIView.as_view())),
+    url(r'^api/(\w+)/(\w+))', csrf_exempt(api_renderer_views.APIView.as_view())),
+    url(r'^api/(\w+)', csrf_exempt(api_renderer_views.APIView.as_view())),
+    url(r'^api/', csrf_exempt(api_renderer_views.APIView.as_view())),
 )
