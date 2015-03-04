f69a95b6b247d5e1e088ffc5952cb8633ddfcd6a
David Gillies <david.gillies.rhu@gmail.com>
2015-03-04 16:47:13 +0000

    added in beautiful soup midleware to tidy html output and make it easier to read

--- fenland_api/fenland_api/settings.py
+++ fenland_api/fenland_api/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'html_renderer.middleware.BeautifulMiddleware',
 )
 
 ROOT_URLCONF = 'fenland_api.urls'
