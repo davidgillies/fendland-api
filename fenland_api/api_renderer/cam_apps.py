1ad52ff24fe2b2b66e05e5a69f9cabf924780223
David Gillies <david.gillies.rhu@gmail.com>
2015-03-10 16:37:25 +0000

    pep8 tidy up and some comments

--- fenland_api/api_renderer/cam_apps.py
+++ fenland_api/api_renderer/cam_apps.py
@@ -7,16 +7,18 @@ from copy import deepcopy
 class VariableType(object):
     def __init__(self, variable):
         pass
-    
+
+
 class MethodMixin(object):
     def set_rendering_hint(self, item):
         rh = {}
-        #rh['type'] = item.rhType
+        # rh['type'] = item.rhType
         rh[item.rhType] = ''
         for rhdata in item.rhData:
             rh[item.rhType] = rh[item.rhType] + ' ' + str(rhdata)
         rh[item.rhType] = rh[item.rhType].strip()
         self.rendering_hints.append(rh)
+
 
 class Question(MethodMixin):
     def __init__(self, question_object):
@@ -28,11 +30,11 @@ class Question(MethodMixin):
         self.position = question_object.attrib['position']
         self.rendering_hints = []
         self.build_question(question_object)
-        
+
     def build_question(self, question_object):
         for item in question_object.getchildren():
             self.tag_type(item.tag)(item)
-        
+
     def tag_type(self, tag_type):
         return {'{http://www.mrc-epid.cam.ac.uk/schema/common/epi}title': self.set_title, 
                 '{http://www.mrc-epid.cam.ac.uk/schema/common/epi}info': self.set_info,
@@ -42,29 +44,32 @@ class Question(MethodMixin):
                 '{http://www.mrc-epid.cam.ac.uk/schema/common/epi}restrictions': self.set_restrictions,
                 '{http://www.mrc-epid.cam.ac.uk/schema/common/epi}variable': self.set_variable
                 }[tag_type]
-                
+
     def set_restrictions(self, item):
         pass
-    
+
     def set_options(self, item):
         pass
-    
+
     def set_variable(self, item):
         pass
-        
+
     def set_info(self, item):
         q_info = {}
         q_info['text'] = item.text
         q_info['cssClass'] = item.attrib['cssClass']
         self.question_objects.append(q_info)
-        
+
     def set_title(self, item):
         return
 
-        
     def set_external_programs(self, item):
         pass
 
+# Tod dos:
+# 1. set table and shownumber on the QuestionGroup object, they come from
+# the renderingHints.  
+# 2. Refactor rendering_hints stuff
 class QuestionGroup(MethodMixin):
     def __init__(self, question_group_object):
         self.question_group_objects = []
@@ -75,7 +80,7 @@ class QuestionGroup(MethodMixin):
     def build_question_group(self, question_group_object):
         for item in question_group_object.getchildren():
             self.tag_type(item.tag)(item)
-            
+
     def tag_type(self, tag_type):
         return {'{http://www.mrc-epid.cam.ac.uk/schema/common/epi}title': self.set_title, 
                 '{http://www.mrc-epid.cam.ac.uk/schema/common/epi}info': self.set_info,
@@ -84,16 +89,16 @@ class QuestionGroup(MethodMixin):
                 '{http://www.mrc-epid.cam.ac.uk/schema/common/epi}question': self.set_question,
                 '{http://www.mrc-epid.cam.ac.uk/schema/common/epi}externalPrograms': self.set_external_programs
                 }[tag_type]
-                
+
     def set_title(self, item):
         return
-        
+
     def set_info(self, item):
         qg_info = {}
         qg_info['text'] = item.text
         qg_info['cssClass'] = item.attrib['cssClass']
         self.question_group_objects.append(qg_info)
-    
+
     def set_text_node(self, item):
         text_node = {}
         try:
@@ -111,16 +116,14 @@ class QuestionGroup(MethodMixin):
                 rh_item['data'].append(rhdata)
             text_node['rendering_hints'].append(rh_item)
         self.question_group_objects.append(text_node)
-        
-    
+
     def set_question(self, item):
         question = Question(item)
         self.question_group_objects.append(question)
 
-
-        
     def set_external_programs(self, item):
         pass
+
 
 class Section(MethodMixin):
     def __init__(self, section_xml_object):
@@ -133,8 +136,8 @@ class Section(MethodMixin):
         self.build_section()
 
     def build_section(self):
-        for item in section.getchildren():
-            func = self.tag_type(item.tag)(item)
+        for item in self.section_xml_object.getchildren():
+            self.tag_type(item.tag)(item)
 
     def tag_type(self, tag_type):
         return {'{http://www.mrc-epid.cam.ac.uk/schema/common/epi}title': self.set_title, 
@@ -146,17 +149,17 @@ class Section(MethodMixin):
 
     def set_title(self, item):
         return
-        
+
     def set_renderering_hint(self, item):
         pass
-        
+
     def set_info(self, item):
         section_info = {}
         section_info['text'] = item.text
         section_info['cssClass'] = item.attrib['cssClass']
         self.info.append(section_info)
         self.section_objects.append(section_info)
-        
+
     def set_question_group(self, item):
         question_group = QuestionGroup(item)
         self.question_groups.append(question_group)
@@ -164,6 +167,7 @@ class Section(MethodMixin):
 
     def set_external_programs(self, item):
         pass
+
 
 class Application(object):
     def __init__(self, name, xml):
