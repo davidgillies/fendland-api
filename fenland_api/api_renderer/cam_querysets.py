03f2553ec69549513070767bd7fccbd49c372d0b
David Gillies <david.gillies.rhu@gmail.com>
2015-04-16 14:44:06 +0100

    added functionality to queryset to allow related sets to be updated/created

--- fenland_api/api_renderer/cam_querysets.py
+++ fenland_api/api_renderer/cam_querysets.py
@@ -8,18 +8,23 @@ db = sqlsoup.SQLSoup(local_settings.DATABASE)
 
 class QuerySet(object):
     def __init__(self, query_dict={}, table_name='', sql='',
-                 id_variable_value='', related_table=''):
+                 id_variable_value='', related_table='', related_field=''):
         self.query_dict = query_dict
         self.table_name = table_name
         self.sql = sql
-        self.related_table = related_table
+        self.related_field = related_field
+        self.related_table_name = related_table
         self.db = db
         self.data = None
         self.id_variable_value = id_variable_value
         if self.table_name:
-            self.table = self.db.entity(table_name)
+            self.table = self.db.entity(self.table_name)
         else:
             self.table = None
+        if self.related_table_name:
+            self.related_table = self.db.entity(self.related_table_name)
+        else:
+            self.related_table = None
         self.objects = Bunch({'all': self.all, 'get': self.get,
                               'create': self.create,
                               'update': self.update, 'delete': self.delete,
@@ -36,7 +41,7 @@ class QuerySet(object):
         return result
 
     def get(self, id_variable_value=None):
-        if id_variable_value == None:
+        if id_variable_value is None:
             id_variable_value = self.id_variable_value
         data = self.table.get(int(id_variable_value)).__dict__
         data['id'] = id_variable_value
@@ -44,21 +49,43 @@ class QuerySet(object):
         self.tidy(data)
         return data
 
-    def create(self, query_dict):
+    def create(self, query_dict):        
         data = self.table.insert(**query_dict).__dict__
         data.pop('_sa_instance_state')
         db.commit()
         self.id_variable_value = data[u'id']
         self.data = data
         return data
+    
+    def create_related(self, query_dict):
+        data = self.related_table.insert(**query_dict).__dict__
+        db.commit()
+        data.pop('_sa_instance_state')
+        self.data = data
+        return data
 
     def update(self, query_dict, id_variable_value):
+        relateds = []
+        for key in query_dict.keys():
+            if isinstance(query_dict[key], list):
+                relateds = query_dict.pop(key)
+        for related in relateds:
+            related[self.related_field] = int(id_variable_value)
+            if related.has_key('id'):
+                self.update_related(related, related['id'])
+            else:
+                self.create_related(related)
         self.id_variable_value = int(id_variable_value)
         data = self.table.filter_by(id=int(id_variable_value)).update(query_dict)
         data = query_dict
         db.commit()
         self.data = data
         return
+    
+    def update_related(self, query_dict, id_variable_value):
+        data = self.related_table.filter_by(id=int(id_variable_value)).update(query_dict)
+        db.commit()
+        return 
 
     def delete(self, id_variable_value):
         instance = self.table.get(int(id_variable_value))
@@ -68,7 +95,7 @@ class QuerySet(object):
         return
 
     def sql_execute(self, sql_string):
-        rp = db.execute(sql_string)
+        rp = self.db.execute(sql_string)
         rows = rp.fetchall()
         result = []
         for row in rows:
@@ -81,11 +108,11 @@ class QuerySet(object):
         for k in data.keys():
             if isinstance(data[k], datetime.date):
                 data[k] = str(data[k])
- 
+                
     def filter(self, field, like, order=None):
         result = self.sql_execute("""select * from %s where %s like '%%%s%%';""" % (self.table_name, field, like))
         return result
-
+    
     def related_set(self, related_table=None, field='id'):
         if related_table == None:
             related_table = self.related_table
@@ -97,3 +124,4 @@ class QuerySet(object):
         for key, value in row.items():
             d[key] = value
         return d
+
