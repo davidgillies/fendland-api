381801bb4afcb853c3e7e05c7d613b4937a0adc0
David Gillies <david.gillies.rhu@gmail.com>
2015-04-29 16:55:45 +0100

    added some new stuff to validator, not quite there yet but an improvement

--- fenland_api/api_renderer/validator.py
+++ fenland_api/api_renderer/validator.py
@@ -1,3 +1,6 @@
+import time
+
+
 class Validator(object):
     def __init__(self, questions, data):
         self.data = data
@@ -19,9 +22,6 @@ class Validator(object):
         return {'CheckMaxLength': self.maxlength, 'IsAnswered': self.required,
                 'type': self.test_type}[test]
 
-    def get_type_test(self, the_type):
-        return {'string': str, 'date': str, 'datetime': str}[the_type]
-
     def maxlength(self, question, answer):
         if len(answer) > int(question.maxlength):
             return (False, 'Your answer should be less than %s characters long' % question.maxlength)
@@ -35,4 +35,35 @@ class Validator(object):
             return (True, None)
 
     def test_type(self, question, answer):
+        return self.get_type_test(question.data_type['type'])(answer)
+        
+    def get_type_test(self, the_type):
+        return {'string': self.check_str, 'date': self.check_date, 'datetime': self.check_time}[the_type]
+        
+    def check_date(self, date_value):
+        masks = ['%Y%m%d','%Y-%m-%d','%d%m%Y','%m%d%Y']
+        # change to use question.pattern and use this format in XML
+        return self.check_format(date_value, masks)
+
+    def check_time(self, time_value):
+        masks = ['%H:%M']
+        return self.check_format(time_value, masks)
+        
+    def check_format(self, value, masks):
+        format_ok = False
+        error = 'Incorrect Format'
+        for mask in masks:
+            try:
+                time.strptime(value, mask)
+                format_ok = True
+                error = None
+                break
+            except:
+                pass
+        return (format_ok, error)
+
+    def check_str(self, string_value):
+        return (True, None)
+
+    def check_int(self, string_value):
         return (True, None)
